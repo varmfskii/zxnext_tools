2f55f820700eb1d1df91e2ce343b60b6e50298f1
Theodore (Alex) Evans <taevans@cs.unm.edu>
2019-07-29 12:26:11 -0400

    Add safety checks for baud rate selection: max performance about 133kbps in mode 7

--- zxnftp/server/setbaud.c
+++ zxnftp/server/setbaud.c
@@ -7,15 +7,26 @@ __sfr __banked __at 0x253b NEXT_RW;
 const int32_t clks[]={ 28000000, 28571429, 29464286, 30000000,
 		     31000000, 32000000, 33000000, 27000000 };
 
+#define MINSUB 12
+#define MAXSUB 16383
+
 uint8_t setbaud(int32_t baud) {
   int32_t clk, sub;
   uint8_t r;
   uint16_t i;
-  
+
   NEXT_SEL=0x11;
   clk=clks[NEXT_RW];
   printf("%d.%06d MHz\n", clk/1000000, clk%1000000);
   sub=(clk+(baud>>1))/baud;
+  if (sub<MINSUB) {
+    sub=MINSUB;
+    baud=(clk+(sub>>1))/sub;
+  }
+  if (sub>MAXSUB) {
+    sub=MAXSUB;
+    baud=(clk+(sub>>1))/sub;
+  }
   printf("%ld baud=%ld sub\n", baud, sub);
   sprintf(buf, "AT+UART_CUR=%ld,8,1,0,0\r\n", baud);
   if ((r=cmdresponse(buf))) {
